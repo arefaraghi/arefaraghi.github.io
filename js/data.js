28b1bd452662b5e2d6ca016593b7e172df376818
Mostafa Zeinivand <mostafa.zeinivand@gmail.com>
2021-09-09 15:28:46 +0430

    init

---
+++ js/data.js
@@ -0,0 +1,72 @@
+const headerLinks = [
+    {
+        label: 'Work',
+        href: 'https://arefaraghi.github.io',
+        target: '_self',
+        class: ['bold']
+    },
+    {
+        label: 'About',
+        href: './about.html',
+        target: '_self',
+        class: []
+    },
+    {
+        label: 'Resume',
+        href: 'https://www.dropbox.com/s/8gczd4xbmj5a2sf/Aref%20Ahmadi%20Araghi%20-%20Designer.pdf?dl=0',
+        target: '_blank',
+        class: []
+    },
+    {
+        label: 'Email',
+        href: 'mailto:aref.araghi@gmail.com',
+        target: '',
+        class: []
+    }
+]
+
+const caseStudies = [
+    {
+        title: 'Redesigning a VOD’s app experience',
+        href: 'https://example.com',
+        readMoreLabel: 'Read',
+        image: './images/namava.png'
+    },
+    {
+        title: 'Redesigning My Shatel app',
+        href: 'https://example.com',
+        readMoreLabel: 'Read',
+        image: './images/shatel.png'
+    }
+]
+
+const projects = [
+    {
+        title: 'TrialBlaze dashboard',
+        description: 'Creating complex dashboards is always a challenge. Speaking of dashboards I really enjoyed working on TrialBlaze (an Australian startup) project.',
+        href: 'https://dribbble.com/shots/16307904-TrialBlaze-dashboard',
+        linkLabel: 'Link',
+        image: './images/trial-blaze-dashboard.png'
+    },
+    {
+        title: 'Gift Cards',
+        description: 'Here\'s a couple of gift cards I designed for a gift card shop website.',
+        href: 'https://dribbble.com/shots/9135756-Gift-Cards',
+        linkLabel: 'Link',
+        image: './images/gift-card.png'
+    },
+    {
+        title: 'TrialBlaze Landing',
+        description: 'Here\'s a landing page that I have been working on recently. Our team launched the first version of the the product and now we\'re waiting for some good news. ',
+        href: 'https://dribbble.com/shots/14348806-TRIALBLAZE-Landing-Page',
+        linkLabel: 'Link',
+        image: './images/trial-blaze.png'
+    },
+    {
+        title: 'STARCRAFT Gameplay',
+        description: 'A few years ago, I painted a space marine from STARCRAFT. <br>A design concept I came up with recently is inspired by League of Legends\' stunning design.',
+        href: 'https://dribbble.com/shots/16392339-STARCRAFT-Gameplay',
+        linkLabel: 'Link',
+        image: './images/star-craft.png'
+    }
+]
---
+++ js/scripts.js
@@ -0,0 +1,134 @@
+function createHeaderLinks(links) {
+    const ul = document.getElementById('header-links')
+    const template = document.getElementById('header-link-template')
+
+    links.forEach((link) => {
+        const li = document.importNode(template.content, true)
+        const a = li.querySelector('a')
+
+        a.textContent = link.label
+        a.href = link.href
+
+        if (link.target !== '') {
+            a.target = link.target
+        }
+
+        link.class.forEach((c) => {
+            a.classList.add(c)
+        })
+
+        ul.appendChild(li)
+    })
+
+    ul.replaceWith(ul)
+}
+
+function createCaseStudies(caseStudies) {
+    const container = document.getElementById('case-studies-container')
+    const template = document.getElementById('case-study-template')
+
+    caseStudies.forEach((caseStudy) => {
+        const item = document.importNode(template.content, true)
+
+        item.querySelector('img').src = caseStudy.image
+        item.querySelector('.body strong').textContent = caseStudy.title
+        item.querySelector('.body a').href = caseStudy.href
+        item.querySelector('.body a span').textContent = caseStudy.readMoreLabel
+
+        container.appendChild(item)
+    })
+
+    container.replaceWith(container)
+}
+
+function createProjects(projects) {
+    const container = document.querySelector('#projects-container .siema')
+    const template = document.getElementById('project-template')
+    const totalSlides = projects.length
+    const prev = document.querySelector('.prev')
+    const next = document.querySelector('.next')
+
+    projects.forEach((project) => {
+        const item = document.importNode(template.content, true)
+
+        item.querySelector('img').src = project.image
+        item.querySelector('.body strong').textContent = project.title
+        item.querySelector('.body p').innerHTML = project.description
+        item.querySelector('.body a').href = project.href
+        item.querySelector('.body a').textContent = project.linkLabel
+
+        container.appendChild(item)
+    })
+
+    container.replaceWith(container)
+    document.getElementById('total-pages').textContent = projects.length
+
+    function printSlideIndex() {
+        const currentSlide = this.currentSlide + 1
+
+        document.querySelector('#pagination .prev').classList.remove('disabled')
+        document.querySelector('#pagination .next').classList.remove('disabled')
+
+        document.getElementById('current-page').textContent = currentSlide
+
+        if (currentSlide === 1) {
+            document.querySelector('#pagination .prev').classList.add('disabled')
+        }
+
+        if (currentSlide >= totalSlides) {
+            document.querySelector('#pagination .next').classList.add('disabled')
+        }
+    }
+
+    const siema = new Siema({
+        onInit: printSlideIndex,
+        onChange: printSlideIndex,
+    })
+
+    prev.addEventListener('click', () => siema.prev())
+    next.addEventListener('click', () => siema.next())
+}
+
+function mobileMenu() {
+    const header = document.getElementById('site-header')
+    const overlay = document.getElementById('header-overlay')
+
+    document.getElementById('mobile-menu').addEventListener('click', () => {
+        if (header.classList.contains('active')) {
+            header.classList.remove('active')
+            overlay.classList.remove('active')
+        }
+        else {
+            header.classList.add('active')
+            overlay.classList.add('active')
+        }
+    })
+
+    document.getElementById('header-overlay').addEventListener('click', () => {
+        header.classList.remove('active')
+        overlay.classList.remove('active')
+    })
+}
+
+function responsiveTools() {
+    window.removeEventListener('resize', resize)
+    window.addEventListener('resize', resize)
+
+    function resize() {
+        setTimeout(() => {
+            const width = document.querySelector('#projects-container .project:first-child .media img').offsetHeight;
+
+            document.documentElement.style.setProperty('--project-height', width + 'px')
+        }, 100)
+    }
+
+    resize()
+}
+
+window.onload = (event) => {
+    createHeaderLinks(headerLinks)
+    createCaseStudies(caseStudies)
+    createProjects(projects)
+    mobileMenu()
+    responsiveTools()
+}
